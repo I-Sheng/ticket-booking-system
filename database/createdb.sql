cf58873f5a7852154b11362fcee23397d860e982
wanchichang <a0965266902@gmail.com>
2024-12-13 16:52:57 +0800

    fix: adjust schema and create new test data

--- database/createdb.sql
+++ database/createdb.sql
@@ -1,52 +1,59 @@
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE "users" (
-  "email" varchar PRIMARY KEY,
+  "_id" uuid not null default uuid_generate_v4() PRIMARY KEY,
+  "email" varchar,
   "username" varchar,
+  "password" varchar,
   "role" varchar,
   "phone_number" varchar,
-  "is_disabled" bool,
-  "created_at" timestamp
+  "is_disabled" boolean default false,
+  "created_at" timestamp with time zone not null default now()
 );
 
-CREATE TABLE "password" (
-  "email" varchar PRIMARY KEY,
-  "password" varchar
-);
-
-CREATE TABLE "ticket" (
-  "id" integer PRIMARY KEY,
-  "created_at" timestamp,
-  "owner_email" varchar,
-  "activity_id" integer,
-  "region" varchar,
-  "seat_number" integer,
-  "is_paid" bool
-);
-
-CREATE TABLE "activity" (
-  "id" integer PRIMARY KEY,
-  "on_sale_date" date,
-  "start_time" date[],
-  "end_time" date[],
-  "title" varchar,
-  "content" varchar,
-  "cover_img" file,
-  "price_level_img" file,
-  "arena_id" integer,
-  "region_name" varchar[],
-  "region_price" integer[]
-);
-
-CREATE TABLE "arena" (
-  "id" integer PRIMARY KEY,
+CREATE TABLE "arenas" (
+  "_id" uuid not null default uuid_generate_v4() PRIMARY KEY,
   "title" varchar,
   "address" varchar,
   "capacity" integer
 );
 
-ALTER TABLE "password" ADD FOREIGN KEY ("email") REFERENCES "users" ("email");
+CREATE TABLE "activities" (
+  "_id" uuid not null default uuid_generate_v4() PRIMARY KEY,
+  "on_sale_date" date,
+  "start_time" timestamp,
+  "end_time" timestamp,
+  "title" varchar,
+  "content" varchar,
+  "cover_img" varchar,
+  "price_level_img" varchar,
+  "arena_id" uuid
+);
 
-ALTER TABLE "ticket" ADD FOREIGN KEY ("owner_email") REFERENCES "users" ("email");
+CREATE TABLE "regions" (
+  "_id" uuid not null default uuid_generate_v4() PRIMARY KEY,
+  "activity_id" uuid,
+  "region_name" varchar,
+  "region_price" integer,
+  "region_capacity" integer
+);
 
-ALTER TABLE "ticket" ADD FOREIGN KEY ("activity_id") REFERENCES "activity" ("id");
+CREATE TABLE "tickets" (
+  "_id" uuid not null default uuid_generate_v4() PRIMARY KEY,
+  "user_id" uuid,
+  "created_at" timestamp with time zone not null default now(),
+  "activity_id" uuid,
+  "region_id" uuid,
+  "seat_number" integer,
+  "is_paid" boolean default false
+);
+ALTER TABLE "tickets" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("_id");
 
-ALTER TABLE "activity" ADD FOREIGN KEY ("arena_id") REFERENCES "arena" ("id");
+ALTER TABLE "tickets" ADD FOREIGN KEY ("activity_id") REFERENCES "activities" ("_id");
+
+ALTER TABLE "tickets" ADD FOREIGN KEY ("region_id") REFERENCES "regions" ("_id");
+
+ALTER TABLE "activities" ADD FOREIGN KEY ("arena_id") REFERENCES "arenas" ("_id");
+
+ALTER TABLE "regions" ADD FOREIGN KEY ("activity_id") REFERENCES "activities" ("_id");
