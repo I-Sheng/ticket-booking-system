acf3c16339ca368c4bab9f68dfda63dda2013b5f
isheng <110703011@g.nccu.edu.tw>
2025-01-01 21:52:00 +0800

    feat: add timestamp for database

--- backend/src/database/tickets/update.ts
+++ backend/src/database/tickets/update.ts
@@ -1,60 +1,65 @@
-import { query } from '../database'
+import { query } from "../database";
 
 export async function updateTicket(data: {
-  ticket_id: string
-  user_id?: string
-  is_paid?: boolean
-  seat_number?: number
+  ticket_id: string;
+  user_id: string | undefined | null;
+  is_paid?: boolean;
+  seat_number?: number;
+  created_at?: Date;
 }) {
-  const fields = []
-  const values = []
-  let index = 1
+  const fields = [];
+  const values = [];
+  let index = 1;
 
   if (data.is_paid !== undefined) {
-    fields.push(`is_paid = $${index++}`)
-    values.push(data.is_paid)
+    fields.push(`is_paid = $${index++}`);
+    values.push(data.is_paid);
   }
   if (data.seat_number !== undefined) {
-    fields.push(`seat_number = $${index++}`)
-    values.push(data.seat_number)
+    fields.push(`seat_number = $${index++}`);
+    values.push(data.seat_number);
   }
   if (data.user_id !== undefined) {
-    fields.push(`user_id = $${index++}`)
-    values.push(data.user_id)
+    fields.push(`user_id = $${index++}`);
+    values.push(data.user_id);
+  }
+  if (data.created_at !== undefined) {
+    fields.push(`created_at = $${index++}`);
+    values.push(data.created_at);
   }
 
   if (fields.length === 0) {
-    return { error: 'No fields to update' }
+    return { error: "No fields to update" };
   }
 
   const qstring = `
     UPDATE tickets
-    SET ${fields.join(', ')}
+    SET ${fields.join(", ")}
     WHERE _id = $${index}
     RETURNING *;
-  `
-  values.push(data.ticket_id)
+  `;
+  values.push(data.ticket_id);
 
   try {
     // 開啟交易
-    await query('BEGIN', [])
+    await query("BEGIN", []);
 
-    const result = await query(qstring, values)
+    const result = await query(qstring, values);
 
     if (result.rowCount === 0) {
       // 若找不到票，回滾交易
-      await query('ROLLBACK', [])
-      return { error: 'Ticket not found' }
+      await query("ROLLBACK", []);
+      return { error: "Ticket not found" };
     }
 
     // 提交交易
-    await query('COMMIT', [])
-    return result.rows[0]
+    await query("COMMIT", []);
+    return result.rows[0];
   } catch (error) {
-    console.error('Error updating ticket:', error)
+    console.error("Error updating ticket:", error);
 
     // 若發生錯誤，回滾交易
-    await query('ROLLBACK', [])
-    return { error: 'Failed to update ticket' }
+    await query("ROLLBACK", []);
+    return { error: "Failed to update ticket" };
   }
 }
