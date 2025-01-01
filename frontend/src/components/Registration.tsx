382dc70943d5a913b10c85e7f7d43757335f9bc8
Kai <khsauian@gmail.com>
2025-01-01 14:30:14 +0800

    feat: activity

--- frontend/src/components/Registration.tsx
+++ frontend/src/components/Registration.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-const API_URL = 'http://localhost:8080'
+// const API_URL = 'http://localhost:8080'
+const API_URL = process.env.REACT_APP_API_URL
 
 const Registration: React.FC = () => {
   const [step, setStep] = useState(1)
--- frontend/src/components/activity.tsx
+++ frontend/src/components/activity.tsx
@@ -1,47 +1,84 @@
 // components/activity.tsx
 import React, { useEffect, useState } from 'react'
+import { useAuth } from '../context/AuthContext' // 確保導入 useAuth
 import { useParams } from 'react-router-dom'
+const API_URL = process.env.REACT_APP_API_URL
+
+interface Region {
+  region_name: string
+  region_price: number
+  region_capacity: number
+}
 
 interface Activity {
-  id: number
-  name: string
-  description: string
-  date: string
-  location: string
+  _id: string
+  on_sale_date: string
+  start_time: string
+  end_time: string
+  title: string
+  content: string
+  cover_img: { type: string; data: number[] }
+  price_level_img: { type: string; data: number[] }
+  arena_id: string
+  creator_id: string
+  is_archived: boolean
+  regions: Region[]
 }
 
 const ActivityPage: React.FC = () => {
-  const { id } = useParams()  // 获取活动的 ID
+  const { id } = useParams() // 获取活动的 ID
   const [activity, setActivity] = useState<Activity | null>(null)
-
-  useEffect(() => {
-    if (id) {
-      // 模拟获取活动数据，实际情况应该是通过 API 请求获取
-      const fetchedActivity: Activity = {
-        id: Number(id),
-        name: `活动 ${id}`,
-        description: `这是活动 ${id} 的描述`,
-        date: '2024-12-30',
-        location: '活动地点',
+  const [status, setStatus] = useState<string>('')
+  const fetchActivity = async (id: string) => {
+    try {
+      const response = await fetch(`${API_URL}/activities/get/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (response.ok) {
+        const data = await response.json()
+        console.log('data',data)
+        setStatus('ok')
+        setActivity(data.activity) // 假设后端返回的数据中包含 activity 属性
+      } else {
+        setStatus('error')
+        console.error('Failed to fetch activity:', response.statusText)
       }
-
-      setActivity(fetchedActivity)
+    } catch (error) {
+      setStatus('error')
+      console.error('Error fetching activity:', error)
     }
+  }
+  useEffect(() => {
+    fetchActivity(id!)
   }, [id])
 
   if (!activity) {
-    return <p>活动加载中...</p>
+    return (
+      <>
+        <p>loading...</p>
+        <p>{status}</p>
+      </>
+    )
   }
 
   return (
     <div>
-      <h2>{activity.name}</h2>
-      <p><strong>时间：</strong>{activity.date}</p>
-      <p><strong>地点：</strong>{activity.location}</p>
-      <p>{activity.description}</p>
+      <p>{activity?.title}</p>
+      {/* <h2>{activity.name}</h2> */}
+      {/* <p> */}
+      {/*   <strong>时间：</strong> */}
+      {/*   {activity.date} */}
+      {/* </p> */}
+      {/* <p> */}
+      {/*   <strong>地点：</strong> */}
+      {/*   {activity.location} */}
+      {/* </p> */}
+      {/* <p>{activity.description}</p> */}
     </div>
   )
 }
 
 export default ActivityPage
-
