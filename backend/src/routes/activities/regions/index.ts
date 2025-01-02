010f9cd02b59706df88d3432205a0166e1f66c96
wanchichang <a0965266902@gmail.com>
2025-01-02 09:33:32 +0800

    feat: get region by id

--- backend/src/routes/activities/regions/index.ts
+++ backend/src/routes/activities/regions/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import createRouter from './create'
 import updateRouter from './update'
 import deleteRouter from './delete'
+import getRouter from './get'
 
 const router = express.Router()
 
 router.use('/', createRouter)
 router.use('/', updateRouter)
 router.use('/', deleteRouter)
+router.use('/', getRouter)
 
 export default router
